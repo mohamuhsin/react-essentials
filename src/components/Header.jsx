7c40e3593774e0c9ba48a4e38d6ba660dafe105f
mohamuhsin <muhsinohoho@outlook.com>
2023-11-04 01:46:44 +0300

    created Header Component

---
+++ src/components/Header.jsx
@@ -0,0 +1,12 @@
+function Header() {
+  return (
+    <header>
+      <img src="src/assets/react-core-concepts.png" />
+      <p>
+        Fundamental React concepts you will need for almost any app you are
+        going to build!
+      </p>
+    </header>
+  );
+}
+export default Header;
