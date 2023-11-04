437a2d9b5372c7f36d14662bf37958c76b29ad55
mohamuhsin <muhsinohoho@outlook.com>
2023-11-04 03:13:28 +0300

    using props

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -9,7 +9,7 @@ function Header() {
   const description = reactDescriptions[genRandomInt(2)];
   return (
     <header>
-      <img src={reactImg} />
+      <img src={reactImg} alt="Stylized atom" />
       <h1>React Essentials</h1>
       <p>
         {description} React concepts you will need for almost any app you are
