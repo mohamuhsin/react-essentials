ebecef91ba53b3e2d532f4a98b8d738960de58fc
mohamuhsin <muhsinohoho@outlook.com>
2023-11-06 13:52:39 +0300

    updated css

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import Header from "./components/Header";
+import Header from "./components/Header/Header";
 import CoreConcepts from "./components/CoreConcepts";
 
 import { CORE_CONCEPTS } from "./data";
